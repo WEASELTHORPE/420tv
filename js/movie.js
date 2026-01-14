5533047f1d9b0970aca2d64fde8c12fb5a84df4c
Daniel Garza <dgarz160@stu.southtexascollege.edu>
2026-01-14 17:00:11 -0600

    test

--- js/movie.js
+++ js/movie.js
@@ -28,16 +28,16 @@ async function fetchTVDetails(id) {
     </div>
     
       
-
+<div class="mt-8 w-full aspect-video">
 <iframe 
   src="https://www.vidking.net/embed/tv/${show.id}/1/1?color=e50914&autoPlay=true&nextEpisode=true&episodeSelector=true" 
-  width="150%" 
+  width="100%" 
   height="600" 
   frameborder="0" 
   allowfullscreen>
 
 </iframe>
-
+</div>
 
 
 
